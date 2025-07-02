dcef4c0e46bd7d1298bfef54ad608bcf0a0d28e3
Abdallah Beshary <abdallahuuuio221@gmail.com>
2025-07-03 02:53:16 +0300

    uploaded controller, helpers and modles folders

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI , APIRouter
-import os 
+from helpers.config import get_settings 
 
 base_router = APIRouter(
-    prefix="api/v1", # static prefix before each route 
+    prefix="/api/v1", # static prefix before each route 
     tags=["api_v1"], # organized routes under a specific_tag
-    responses={404: {"description": "Not found"}}
+    # responses={404: {"description": "Not found"}}
 )
 
 @base_router.get("/")
 async def welcome():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+    app_settings = get_settings()
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {
         "message": "Hello ",
         "app_name" : app_name ,
